6c12265500972bf3bfa517a73c58610f55780497
arik <tonoyandeveloper@gmail.com>
2018-03-08 05:44:16 +0300

    Seller.sol struct added

--- contracts/Seller.sol
+++ contracts/Seller.sol
@@ -3,5 +3,21 @@ pragma solidity ^0.4.18;
 import "./Operations.sol";
 
 contract Seller {
+    struct SellerStruct {
+        address CaleroMain;
+
+        // Seller country, name, addressStreet, city, postal code
+        bytes32 country;
+        bytes32 name;
+        bytes32 addressStreet;
+        bytes32 city;
+        bytes32 postalCode;
+
+        // Additional info
+        mapping (address => bool) users;
+        address[] usersList;
+    }
+
+    SellerStruct investor;
 
 }
