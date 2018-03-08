9188f13545b066072a307e4217139bd5110d2e3b
arik <tonoyandeveloper@gmail.com>
2018-03-08 05:32:11 +0300

    Invoice.sol structs added

--- contracts/Invoice.sol
+++ contracts/Invoice.sol
@@ -5,4 +5,43 @@ import "./SafeMath.sol";
 contract Invoice {
     using SafeMath for uint;
 
+    struct SettlementStruct {
+        address seller;
+        address payer;
+        uint amount;
+        uint offerExpiresDate;
+        uint payedOnDate;
+        uint paid;
+    }
+
+    struct InvoiceStruct {
+        address CaleroMain;
+
+        // Company owner
+        address owner;
+        address[] owners;
+
+        // Base info
+        uint invoiceId;
+        uint issueDate;
+        uint payDueDate;
+
+        // Buyer/Seller
+        address seller; // seller
+        address payer; // payer
+
+        // Invoice product/service info
+        string item;
+        uint quantity;
+        uint pricePerUnit;
+        uint amountForPay;
+        string currency;
+        string itemDescription;
+
+        string messageToRecipient;
+
+        uint8 state; // State 0 - Pending, 1 - Finished
+
+        mapping (address => SettlementStruct) settlements;
+    }
 }
