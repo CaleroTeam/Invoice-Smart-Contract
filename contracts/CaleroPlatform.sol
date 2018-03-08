52f8f430d97df8cfacbbf476fef817491ad76edc
arik <tonoyandeveloper@gmail.com>
2018-03-08 05:29:58 +0300

    CaleroPlatform.sol add company / list companies functions added

--- contracts/CaleroPlatform.sol
+++ contracts/CaleroPlatform.sol
@@ -17,7 +17,7 @@ contract CaleroPlatform is Ownable {
     event InvoiceCreated(address invoice);
     event CompanyRegistered(address company);
     event SellerRegistered(address investor);
-    
+
     // Add invoice to platform Invoices list
     function addInvoice(address invoice) public {
         invoices[invoice] = true;
@@ -28,5 +28,20 @@ contract CaleroPlatform is Ownable {
     // List all invoices
     function listInvoices() public constant returns (address[]) {
         return invoicesList;
+    }
+    
+    // Create new company
+    function createCompany(bytes32 country, bytes32 name, bytes32 addressStreet, bytes32 city, bytes32 postalCode) public returns (address) {
+        address company = new Company(msg.sender, address(this), country, name, addressStreet, city, postalCode);
+        companies[company] = true;
+        companiesList.push(company);
+        CompanyRegistered(company); // event
+
+        return company;
+    }
+
+    // List of all registered companies
+    function listCompanies() public constant returns (address[]) {
+        return companiesList;
     }
 }
