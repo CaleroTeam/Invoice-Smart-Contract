931855230b2e65767c4ead632623810bce2d11a0
arik <tonoyandeveloper@gmail.com>
2018-03-08 05:38:57 +0300

    Company.sol main struct added

--- contracts/Company.sol
+++ contracts/Company.sol
@@ -3,5 +3,19 @@ pragma solidity ^0.4.18;
 import "./Operations.sol";
 
 contract Company is Operations {
+    struct CompanyStruct {
+        // Main smart contract address
+        address CaleroMain;
 
+        // Company country, name, addressStreet, city, postal code
+        bytes32 country;
+        bytes32 addressStreet;
+        bytes32 name;
+        bytes32 city;
+        bytes32 postalCode;
+
+        // Additional info
+        mapping (address => bool) users;
+        address[] usersList;
+    }
 }
