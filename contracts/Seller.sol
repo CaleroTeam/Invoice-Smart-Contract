7a2c6ab17683521863a96c828bce715349d9c2d3
arik <tonoyandeveloper@gmail.com>
2018-03-08 21:14:18 +0300

    Fixes before test

--- contracts/Seller.sol
+++ contracts/Seller.sol
@@ -34,60 +34,7 @@ contract Seller is Operations {
         seller.city = city;
         seller.postalCode = postalCode;
     }
-
-    struct InvoiceDetails {
-        address seller;
-        address payer;
-        uint invoiceId;
-        uint payDueDate;
-        string item;
-        uint quantity;
-        uint pricePerUnit;
-        uint amountForPay;
-        string currency;
-        string itemDescription;
-        string messageToRecipient;
-    }
-
-    // Create new Invoice for company
-    function createInvoice(
-        address _payer,
-        uint _invoiceId,
-        uint _payDueDate,
-        string _item,
-        uint _quantity,
-        uint _pricePerUnit,
-        uint _amountForPay,
-        string _currency,
-        string _itemDescription,
-        string _messageToRecipient)
-    public onlyOwner {
-        InvoiceDetails memory invoiceDetails;
-
-        invoiceDetails.seller = address(this);
-        invoiceDetails.payer = _payer;
-        invoiceDetails.invoiceId = _invoiceId;
-        invoiceDetails.payDueDate = _payDueDate;
-        invoiceDetails.item = _item;
-        invoiceDetails.quantity = _quantity;
-        invoiceDetails.pricePerUnit = _pricePerUnit;
-        invoiceDetails.amountForPay = _amountForPay;
-        invoiceDetails.currency = _currency;
-        invoiceDetails.itemDescription = _itemDescription;
-        invoiceDetails.messageToRecipient = _messageToRecipient;
-
-        createInvoiceCall(invoiceDetails);
-    }
-
-    function createInvoiceCall(InvoiceDetails invoiceDetails) private returns (address) {
-        address invoice = new Invoice(invoiceDetails.seller, invoiceDetails.payer, invoiceDetails.invoiceId, invoiceDetails.payDueDate, invoiceDetails.item, invoiceDetails.quantity, invoiceDetails.pricePerUnit, invoiceDetails.amountForPay, invoiceDetails.currency, invoiceDetails.itemDescription, invoiceDetails.messageToRecipient, seller.CaleroMain);
-
-        CaleroPlatform calero = CaleroPlatform(seller.CaleroMain);
-        calero.addInvoice(invoice);
-
-        return invoice;
-    }
-
+    
     // Modifiers
     modifier onlyOwner() {
         require(isOwner(msg.sender));
