52d5795a44475cf53da857a11f2bf50092816006
arik <tonoyandeveloper@gmail.com>
2018-03-08 05:28:16 +0300

    CaleroPlatform.sol variables init

--- contracts/CaleroPlatform.sol
+++ contracts/CaleroPlatform.sol
@@ -3,5 +3,18 @@ pragma solidity ^0.4.18;
 import "./Ownable.sol";
 
 contract CaleroPlatform is Ownable {
+    uint nextInvoiceId = 0;
 
+    mapping (address => bool) invoices;
+    address[] invoicesList;
+
+    mapping (address => bool) companies;
+    address[] companiesList;
+
+    mapping (address => bool) investors;
+    address[] investorsList;
+
+    event InvoiceCreated(address invoice);
+    event CompanyRegistered(address company);
+    event SellerRegistered(address investor);
 }
